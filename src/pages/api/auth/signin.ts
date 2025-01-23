4fa1f69c834a17233235d6f6a113a69ed47ceb46
faaiz251 <mohdfaaizazeem@gmail.com>
2025-01-24 01:04:09 +0530

    commit

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,21 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Signin }  from '@/pages/signin'
- 
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Signin } from "@/pages/signin";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
     const { email, password } = req.body;
-    Signin('credentials', { email, password });
-  
+    Signin("credentials", { email, password });
+
     res.status(200).json({ success: true });
   } catch (error) {
     // Narrow down the type of error
-    if (error instanceof Error && error.name === 'CredentialsSignin') {
-      res.status(401).json({ error: 'Invalid credentials.' });
+    if (error instanceof Error && error.name === "CredentialsSignin") {
+      res.status(401).json({ error: "Invalid credentials." });
     } else {
-      res.status(500).json({ error: 'Something went wrong.' });
+      res.status(500).json({ error: "Something went wrong." });
     }
   }
 }
